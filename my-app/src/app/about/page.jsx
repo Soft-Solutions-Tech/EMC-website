af223177ad420f6819eed58d2db93d192022cec6
Youssef Metry <youssef.kamal.fawzy@gmail.com>
2025-07-19 21:18:50 +0300

    core values section - Not fully finished

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -3,12 +3,14 @@
 import MeetOurTeam from "../sections/MeetOurTeam";
 import OurActivities from "../sections/OurActivities";
 import WhoWeAreSection from "../sections/WhoWeAreSection";
+import CoreValues from "../sections/CoreValues";
 
 // Main component
 export default function AboutPage() {
   return (
     <>
       <WhoWeAreSection />
+      <CoreValues />
       <MeetOurTeam />
       <OurActivities />
     </>
