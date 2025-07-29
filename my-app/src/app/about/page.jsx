ef3cf893b9bfec591840f2842cc1fbe20c588b06
Youssef Metry <youssef.kamal.fawzy@gmail.com>
2025-07-29 18:33:53 +0300

    commented some sections in the about page

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -1,18 +1,18 @@
 "use client";
 
-import MeetOurTeam from "../sections/MeetOurTeam";
-import OurActivities from "../sections/OurActivities";
+// import MeetOurTeam from "../sections/MeetOurTeam";
+// import OurActivities from "../sections/OurActivities";
 import WhoWeAreSection from "../sections/WhoWeAreSection";
-import CoreValues from "../sections/CoreValues";
+// import CoreValues from "../sections/CoreValues";
 
 // Main component
 export default function AboutPage() {
   return (
     <>
       <WhoWeAreSection />
-      <CoreValues />
+      {/* <CoreValues />
       <MeetOurTeam />
-      <OurActivities />
+      <OurActivities /> */}
     </>
   );
 }
