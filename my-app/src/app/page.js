c1ecdcae87f4f29377606a87df00e34934b86bd5
Youssef Metry <youssef.kamal.fawzy@gmail.com>
2025-07-29 18:29:45 +0300

    change the import msg from Clients to clients

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -2,7 +2,7 @@ import { HeroBanner } from "./sections/HeroBanner";
 import TimelineSection from "./sections/Timeline";
 import InfoSection from "./sections/Info";
 import PortoflioSection from "./sections/Portoflio";
-import ClientsSection from "./sections/Clients";
+import ClientsSection from "./sections/clients";
 
 export default function Home() {
   return (
